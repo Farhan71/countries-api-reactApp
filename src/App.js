4481b6630e24f5e95d17c5672aa52df2464a072b
Farhan71 <farhanitnstu@gmail.com>
2021-04-28 14:20:49 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
+import Population from './components/Population/Population';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(()=> {
+      fetch('https://restcountries.eu/rest/v2/all')
+      .then ( res => res.json())
+      .then (data=> setCountries(data))
+      .catch (err => console.log(err))
+  },[])
+
+  const handleAddCountry = (country)=> {
+    const newCart = [...cart, country]
+    setCart(newCart);
+  }
+
+  //  const names = countries.map()
+
   return (
     <div className="App">
+      <h1>Countries Loaded: {countries.length}</h1>
+      <h3>Country Added: {cart.length}</h3>
+     <Population cart={cart}></Population>
+      
+        {
+          countries.map (country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country> )
+        }
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Country/Country.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Country = (props) => {
+    // console.log (props.country.name)
+    console.log(props)
+    const  {name, population, flag} = props.country
+    const handleAddCountry = props.handleAddCountry;
+    const flagStyle = {
+        height: "50px",
+        border: "2px solid black"
+    }
+    const countryStyle = {
+        border: "2px solid black",
+        borderRadius: "15px",
+        boxShadow: "10px 10px 2px #ccc",
+        margin: "20px",
+        padding: "5px",
+        height: "200px"
+    }
+    return (
+        <div style={countryStyle}>
+            {/* This is {props.name}  */}
+            <img style={flagStyle} src={flag} alt=""/>
+            <h2>{name}</h2>
+            <h4>{population}</h4> 
+            <button onClick={()=> handleAddCountry(props.country)}>Add This</button>
+        </div>
+    );
+};
+
+export default Country;
